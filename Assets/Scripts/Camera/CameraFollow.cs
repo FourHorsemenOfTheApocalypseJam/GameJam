872919cd28bcaf7470d93bba30bfd513b42edf11
Oğuz SEKMEN <78972141+Oguzsekmen@users.noreply.github.com>
2021-04-17 14:37:45 +0300

    Camera

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject player;
+    [SerializeField]
+    float deneme;
+
+    private void Update()
+    {
+        gameObject.transform.position = new Vector3(player.transform.position.x, 6, player.transform.position.z-deneme);
+    }
+}
