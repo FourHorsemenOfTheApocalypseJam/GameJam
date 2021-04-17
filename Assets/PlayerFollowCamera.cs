1b406ea608139c9436d95946869fc4aaea463fe0
Doğuhan <58668943+12ByteCivilEngineer@users.noreply.github.com>
2021-04-17 22:45:49 +0300

    1.st merge

---
+++ Assets/PlayerFollowCamera.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState { None , Falling , Driving , RocketFlying}
+public class PlayerFollowCamera : MonoBehaviour
+{
+    private PlayerState currentPlayerState;
+    Vector3 playerPos;
+    SkyFall player;
+    [SerializeField] Vector2 zBoundaries = new Vector2(1.5f, 9f);
+    [SerializeField] Vector3 fallScreenBoundaries = new Vector3(5.5f, 7f, 1f);
+    //[SerializeField] Vector3 fallCameraLookAt = new Vector3(0f, 0f, 0f);
+    public PlayerState CurrentPlayerState
+    {
+        get
+        {
+            return currentPlayerState;
+        }
+        set
+        {
+            currentPlayerState = value;
+        }
+    }
+
+    private void Awake()
+    {
+        player = FindObjectOfType<SkyFall>();
+    }
+
+    void Start()
+    {
+        currentPlayerState = PlayerState.Falling;
+    }
+
+    void LateUpdate()
+    {
+        playerPos = player.transform.position;
+        ManageFollowCamera();
+    }
+
+    private void ManageFollowCamera()
+    {
+        if (currentPlayerState == PlayerState.None)
+        {
+            return;
+        }
+        else if (currentPlayerState == PlayerState.Falling)
+        {
+            float xTarget = transform.position.x;
+            float yTarget = playerPos.y + fallScreenBoundaries.y;
+            float zTarget = transform.position.z;
+            if (playerPos.x-transform.position.x > fallScreenBoundaries.x)
+            {
+                xTarget = playerPos.x - fallScreenBoundaries.x;
+            }
+            else if (playerPos.x - transform.position.x < -fallScreenBoundaries.x)
+            {
+                xTarget = playerPos.x + fallScreenBoundaries.x;
+            }
+            if (playerPos.z - transform.position.z > zBoundaries.y)
+            {
+                zTarget = playerPos.z - zBoundaries.y;
+            }
+            else if (transform.position.z - playerPos.z > zBoundaries.x)
+            {
+                zTarget = playerPos.z + zBoundaries.x;
+            }
+            transform.position = new Vector3(xTarget,yTarget,zTarget);
+        }
+    }
+}
--- Assets/SkyFall.cs
+++ Assets/SkyFall.cs
@@ -4,15 +4,103 @@ using UnityEngine;
 
 public class SkyFall : MonoBehaviour
 {
+    Rigidbody body;
+    [SerializeField] float maxVertSpeed = -10f;
+    [SerializeField] float gravity = -10f;
+    [SerializeField] float xSpeed = 5f;
+    [SerializeField] float zSpeed = 5f;
+    float xInput = 0f;
+    float yInput = 0f;
+    float horizontalAcceleration = 10f;
+    float minAcceptableVelocity = 0.05f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+        FallSpeedControl();
+        PlayerFallMovement();
+    }
+
+    private void FallSpeedControl()
+    {
+        if (body.velocity.y < maxVertSpeed)
+        {
+            body.AddForce(new Vector3(0f,-gravity/10f,0f), ForceMode.Acceleration);
+        }
+        if (body.velocity.y > 0.95f * maxVertSpeed)
+        {
+            body.AddForce(new Vector3(0f,gravity,0f), ForceMode.Acceleration);
+        }
+    }
+
+    private void PlayerFallMovement()
+    {
+        if (Mathf.Abs(xInput) >= 0.1f)
+        {
+            if (Mathf.Abs(body.velocity.x) < Mathf.Abs(xSpeed))
+            {
+                body.AddForce(new Vector3(xInput * horizontalAcceleration, 0f, 0f), ForceMode.Acceleration);
+            }
+            else
+            {
+                if (body.velocity.x >= 0)
+                {
+                    body.AddForce(new Vector3(-horizontalAcceleration / 10f, 0f, 0f), ForceMode.Acceleration);
+                }
+                else
+                {
+                    body.AddForce(new Vector3(horizontalAcceleration / 10f, 0f, 0f), ForceMode.Acceleration);
+
+                }
+            }
+        }
+        else
+        {
+            if (body.velocity.x > minAcceptableVelocity)
+            {
+                body.AddForce(new Vector3(-horizontalAcceleration /*/ 10f*/, 0f, 0f), ForceMode.Acceleration);
+            }
+            else if(body.velocity.x < minAcceptableVelocity)
+            {
+                body.AddForce(new Vector3(horizontalAcceleration /*/ 10f*/, 0f, 0f), ForceMode.Acceleration);
+            }
+        }
+        if (Mathf.Abs(yInput) >= 0.1f)
+        {
+            if (Mathf.Abs(body.velocity.z) < Mathf.Abs(zSpeed))
+            {
+                body.AddForce(new Vector3(0f,0f,yInput* horizontalAcceleration), ForceMode.Acceleration);
+            }
+            else
+            {
+                if (body.velocity.z >= 0)
+                {
+                    body.AddForce(new Vector3(0f,0f,- horizontalAcceleration / 10f), ForceMode.Acceleration);
+                }
+                else
+                {
+                    body.AddForce(new Vector3(0f,0f, horizontalAcceleration / 10f), ForceMode.Acceleration);
+
+                }
+            }
+        }
+        else
+        {
+            if (body.velocity.z > minAcceptableVelocity)
+            {
+                body.AddForce(new Vector3(0f /*/ 10f*/, 0f, -horizontalAcceleration), ForceMode.Acceleration);
+            }
+            else
+            {
+                body.AddForce(new Vector3(0f /*/ 10f*/, 0f, horizontalAcceleration), ForceMode.Acceleration);
+            }
+        }
     }
 }
