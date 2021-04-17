028ef4ab73b2b6b641efd0abf968dbc3fb7a26bb
Oğuz SEKMEN <78972141+Oguzsekmen@users.noreply.github.com>
2021-04-18 01:20:49 +0300

    Oguz

---
+++ Assets/Scripts/Player/CarTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarTrigger : MonoBehaviour
+{
+    [SerializeField]
+    ParticleSystem particle=null;
+
+    private void Start()
+    {
+       
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            particle.Play();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Car"))
+        {
+            particle.Play();
+        }
+    }
+}
--- Assets/Scripts/Player/MotorController.cs
+++ Assets/Scripts/Player/MotorController.cs
@@ -7,7 +7,9 @@ public class MotorController : MonoBehaviour
     float horizontalMove;
     float verticalMove;
     CharacterController player;
-    public float speed;    
+    public float speed;
+    bool gameover = false;
+    UIManager uiManager;
 
     private void Start()
     {
@@ -16,34 +18,56 @@ public class MotorController : MonoBehaviour
     private void Update()
     {
         horizontalMove = Input.GetAxis("Horizontal");
-        verticalMove = Input.GetAxis("Vertical");      
+        verticalMove = Input.GetAxis("Vertical");
+        uiManager = FindObjectOfType<UIManager>();
         
     }
     private void FixedUpdate()
     {
-        //transform.position = transform.position + new Vector3(0,0,1*Time.deltaTime);
-        player.Move(new Vector3(horizontalMove * 1, 0, 5) * (3 * Time.deltaTime));
+        if (!gameover)
+        {
+            player.Move(new Vector3(horizontalMove * 1, 0, 5) * (3 * Time.deltaTime));
 
-        if (Input.GetAxis("Vertical")>0)
-        {
-            player.Move(new Vector3(horizontalMove * 1, 0, verticalMove) * (speed * Time.deltaTime));            
+            if (Input.GetAxis("Vertical") > 0)
+            {
+                player.Move(new Vector3(horizontalMove * 1, 0, verticalMove) * (speed * Time.deltaTime));
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.rotation = Quaternion.Euler(0, 30, 0);
+            }
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.rotation = Quaternion.Euler(0, -30, 0);
+            }
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+
         }
-        if (Input.GetKey(KeyCode.D))
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bomb"))
         {
-            transform.rotation = Quaternion.Euler(0, 30, 0);
-        }
-        if (Input.GetKeyUp(KeyCode.D))
-        {
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.rotation = Quaternion.Euler(0, -30, 0);
-        }
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            transform.rotation = Quaternion.Euler(0, 0, 0);
+            player.Move(new Vector3(0, 10, 0));
+            transform.rotation = Quaternion.Euler(-30,0,0);
+            gameover = true;
+            StartCoroutine(GameOver());
         }
     }
-    
+
+    IEnumerator GameOver()
+    {
+        yield return new WaitForSecondsRealtime(5f);
+        uiManager.GameOver();
+
+    }
+
 }
