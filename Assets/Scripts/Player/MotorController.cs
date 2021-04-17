8e327615b70bf82c7dbfeb3ad0a1ff461e336049
Oğuz SEKMEN <78972141+Oguzsekmen@users.noreply.github.com>
2021-04-17 12:14:13 +0300

    Revert "Oguz"
    This reverts commit ff3b6f4ab5baa9aff4f46513aeeedcfe39e99e7a.

---
+++ Assets/Scripts/Player/MotorController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotorController : MonoBehaviour
+{
+    float horizontalMove;
+    float verticalMove;
+    CharacterController playerController;
+    public float speed;
+    public float move;
+
+    private void Start()
+    {
+        playerController = GetComponent<CharacterController>();        
+    }
+    private void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+        playerController.transform.Rotate(0, 0, 0);
+       
+        
+    }
+    private void FixedUpdate()
+    {
+        playerController.Move(new Vector3(horizontalMove*1, 0, verticalMove)*(speed*Time.deltaTime));
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            Debug.Log("sol");
+            playerController.transform.Rotate(0, 0, move * Time.deltaTime);            
+        }
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            Debug.Log("sağ");
+            playerController.transform.Rotate(0, 0, -move * Time.deltaTime);
+        }
+    }
+}
